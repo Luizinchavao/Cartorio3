318ac9dd4817157a931d2ffdf620d754bd2d9a04
Luiz Gomez <luizinchavao@gmail.com>
2024-06-29 01:58:50 -0300

    Módulo 3 - Aula 5 -Projeto com repetições - Switch case

--- cartorio3.c
+++ cartorio3.c
@@ -3,49 +3,52 @@
 #include <locale.h> // biblioteca de alocações de texto por região
 
 
-int main () 
-{ 
+int main ()  
+{
     int opcao=0; //Definindo variáveis que equivale a 0. // 1 igual(=) esta atribuindo o valor
     int laco=1;
+    
     for (laco=1;laco=1;)
     {
-	    system("cls");
-        setlocale(LC_ALL, "portuguese"); //Definindo a linguagem que podemos usar acentos  
+	     system("cls");
+         setlocale(LC_ALL, "portuguese"); //Definindo a linguagem que podemos usar acentos  
 	      
 	      
-        printf("### Cartório da EBAC ###\n\n"); //Início do menu
-        printf("Escolha a Opção desejada no menu: \n\n");
-        printf("\t1 - Registrar nomes:\n");
-        printf("\t2 - Consultar nomes:\n");
-        printf("\t3 - Deletar nomes:\n\n\n");
-	    printf("Opção:"); // FIm do menu 
+         printf("### Cartório da EBAC ###\n\n"); //Início do menu
+         printf("Escolha a Opção desejada no menu: \n\n");
+         printf("\t1 - Registrar nomes:\n");
+         printf("\t2 - Consultar nomes:\n");
+         printf("\t3 - Deletar nomes:\n\n\n");
+	     printf("Opção:"); // FIm do menu 
         
-        scanf("%d", &opcao); //Armazenando  a escolha do usuario/Armazenando na variavel opcao o valor que o usuario digitar/
+         scanf("%d", &opcao); //Armazenando  a escolha do usuario/Armazenando na variavel opcao o valor que o usuario digitar/
       
-        system("cls"); // O sistema vai deletar todas as mensagens limpar a tela
-        
-       
-		
+         system("cls"); // O sistema vai deletar todas as mensagens limpar a tela
+         
+         switch(opcao) // Computador Analisa essa variavel pra mim 
+        {
+        	case 1: // Caso a opçao for 1
+        	printf("Você escolheu o registro de nomes!\n");
+			system("pause"); // Pausa o sistema no momento
+			break;
+			
+			case 2: 
+			printf("Você escolheu consultar os nomes!\n");
+			system("pause");
+			break;
+			
+			case 3:
+		    printf("Você escolheu deletar os nomes!\n"); 
+		    system("pause");
+			break;
+			
+			default:
+			printf("Essa Opção não está disponível!\n");
+			system("pause");
+			break;
+			
+        }//Fim da seleçao
+    }
+}
       
-        if(opcao==1) // Início da Seleçao // 2 Igual == Esta fazendo uma comparaçao
-        {
-           printf("Você escolheu o registro de nomes!\n");
-        }  system("pause");
-        if(opcao==2)	
-        {
-	       printf("Você escolheu consultar os nomes!\n");
-	    }  system("pause");
-        if(opcao==3) // Sempre vai perguntando para o pc ex:  if= se a variavel opcao que é igual = 3 . Entao computador você fala para o usuario no printf
-        {
-           printf("Você escolheu deletar os nomes!\n"); // Ponto e virgula = ; sempre usar qundo for uma instruçao para o computador
-	    }  system("pause");
-        if(opcao >=4 || opcao <=0) // || essa tecla chama-se PAIPE significa Ou, EX: computador se o valor que o usuario digitar for >=4 ou <=0 fala o que esta no printf
-        {
-      	   printf("Essa Opção não está disponível!\n");
-      	   system("pause");
-        }// fim da seleçao
-	 }       
-     	  
-} 
-   
-   
+         
